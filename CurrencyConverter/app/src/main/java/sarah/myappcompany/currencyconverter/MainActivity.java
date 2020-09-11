514a2bdbb162947bee08c2440cabe41d82ce86c2
Sarah Hart Landolt <sarah.hart.landolt@gmail.com>
2020-09-11 10:12:28 -0500

    finished CurrencyConverter

---
+++ CurrencyConverter/app/src/main/java/sarah/myappcompany/currencyconverter/MainActivity.java
@@ -0,0 +1,31 @@
+package sarah.myappcompany.currencyconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void convertCurrency(View view) {
+        Log.i("Info", "Button Pressed");
+
+        EditText editText= (EditText) findViewById(R.id.editText);
+
+        String amountInPounds = editText.getText().toString();
+
+        double amountInPoundsDouble = Double.parseDouble(amountInPounds);
+        double amountInDollarsDouble= amountInPoundsDouble * 1.3;
+        String amountInDollarsString = String.format("%.2f", amountInDollarsDouble);
+        Toast.makeText(this, " £ " + amountInPounds + " is $ " + amountInDollarsString, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
