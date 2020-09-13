618d35441352d837a417c67d67b34f7dd312d438
Sarah Hart Landolt <sarah.hart.landolt@gmail.com>
2020-09-13 14:27:07 -0500

    completed english to french

---
+++ EnglishToFrench/app/src/main/java/sarah/myappcompany/englishtofrench/MainActivity.java
@@ -0,0 +1,30 @@
+package sarah.myappcompany.englishtofrench;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.media.MediaPlayer;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void playPhrase(View view) {
+
+        Button buttonPressed = (Button) view;
+
+        Log.i("Button pressed", buttonPressed.getTag().toString());
+
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, getResources().getIdentifier(buttonPressed.getTag().toString(), "raw", getPackageName()));
+
+        mediaPlayer.start();
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
